389edd8cdfab7cebb5455d12fc45fe2e735ebd43
Hlib Terentiev <nicknema13@gmail.com>
2023-05-31 15:46:02 +0300

    return usecases, add static code

--- stocks/usecases/portfolio.go
+++ stocks/usecases/portfolio.go
@@ -90,51 +90,51 @@ func (i PortfolioInteractor) CountPortfolio(ctx context.Context, userID string, 
 		return stocks.PortfolioState{}, fmt.Errorf("count portfolio: %w", err)
 	}
 
-	return state, nil
-	//now := time.Now()
-	//today := time.Date(now.Year(), now.Month(), now.Day(), 0,0,0,0, time.UTC)
+	//return state, nil
+	now := time.Now()
+	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
 
-	//rates, err := i.exchangeRater.FindRates(ctx, exchanger.ConvertCurrencyParams{
-	//	ConvertFrom: ep.ConverFrom,
-	//	ConvertTo: ep.ConvertTo,
-	//	Start: today,
-	//	End: today,
-	//})
-	//if err != nil {
-	//	return stocks.PortfolioState{}, fmt.Errorf("find rate: %w", err)
-	//}
-	//
-	//if len(rates) != 1 {
-	//	return stocks.PortfolioState{}, fmt.Errorf("find rate: %w", err)
-	//}
+	rates, err := i.exchangeRater.FindRates(ctx, exchanger.ConvertCurrencyParams{
+		ConvertFrom: ep.ConverFrom,
+		ConvertTo:   ep.ConvertTo,
+		Start:       today,
+		End:         today,
+	})
+	if err != nil {
+		return stocks.PortfolioState{}, fmt.Errorf("find rate: %w", err)
+	}
 
-	//rate := rates[0]
-	//res := stocks.PortfolioState{
-	//	Total: stocks.PortfolioTickerState{
-	//		TickerID: state.Total.TickerID,
-	//		Name: state.Total.Name,
-	//		Description: state.Total.Description,
-	//		Amount: state.Total.Amount,
-	//		High: state.Total.High * rate.Purchase,
-	//		Low: state.Total.Low * rate.Purchase,
-	//		Open: state.Total.Open * rate.Purchase,
-	//		Close: state.Total.Close * rate.Purchase,
-	//	},
-	//	All: make([]stocks.PortfolioTickerState, 0, len(state.All)),
-	//}
-	//
-	//for _, ps := range state.All {
-	//	res.All = append(res.All, stocks.PortfolioTickerState{
-	//		TickerID: ps.TickerID,
-	//		Name: ps.Name,
-	//		Description: ps.Description,
-	//		Amount: ps.Amount,
-	//		High: ps.High * rate.Purchase,
-	//		Low: ps.Low * rate.Purchase,
-	//		Open: ps.Open * rate.Purchase,
-	//		Close: ps.Close * rate.Purchase,
-	//	})
-	//}
+	if len(rates) != 1 {
+		return stocks.PortfolioState{}, fmt.Errorf("find rate: %w", err)
+	}
 
-	//return res, nil
+	rate := rates[0]
+	res := stocks.PortfolioState{
+		Total: stocks.PortfolioTickerState{
+			TickerID:    state.Total.TickerID,
+			Name:        state.Total.Name,
+			Description: state.Total.Description,
+			Amount:      state.Total.Amount,
+			High:        state.Total.High * rate.Purchase,
+			Low:         state.Total.Low * rate.Purchase,
+			Open:        state.Total.Open * rate.Purchase,
+			Close:       state.Total.Close * rate.Purchase,
+		},
+		All: make([]stocks.PortfolioTickerState, 0, len(state.All)),
+	}
+
+	for _, ps := range state.All {
+		res.All = append(res.All, stocks.PortfolioTickerState{
+			TickerID:    ps.TickerID,
+			Name:        ps.Name,
+			Description: ps.Description,
+			Amount:      ps.Amount,
+			High:        ps.High * rate.Purchase,
+			Low:         ps.Low * rate.Purchase,
+			Open:        ps.Open * rate.Purchase,
+			Close:       ps.Close * rate.Purchase,
+		})
+	}
+
+	return res, nil
 }
--- stocks/usecases/stock.go
+++ stocks/usecases/stock.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/glbter/currency-ex/currency/exchanger"
+	"github.com/glbter/currency-ex/pkg/serrors"
 	sqlc "github.com/glbter/currency-ex/pkg/sql"
 	"github.com/glbter/currency-ex/stocks"
 )
@@ -40,39 +41,39 @@ func (i TickerInteractor) QueryLatestDaily(
 		return nil, fmt.Errorf("query daily: %w", err)
 	}
 
-	return tickerDaily, nil
+	//return tickerDaily, nil
 
-	//res := make([]stocks.TickerWithData, 0, len(tickerDaily))
-	//for _, d := range tickerDaily {
-	//	rates, err := i.exchangeRater.FindRates(ctx, exchanger.ConvertCurrencyParams{
-	//		ConvertFrom: ep.ConverFrom,
-	//		ConvertTo: ep.ConvertTo,
-	//		Start: d.DataDate,
-	//		End: d.DataDate,
-	//	})
-	//	if err != nil {
-	//		return nil, fmt.Errorf("find rate: %w", err)
-	//	}
-	//
-	//	// TODO: fix
-	//	if len(rates) != 1 {
-	//		continue
-	//	}
-	//
-	//	rate := rates[0]
-	//
-	//	res = append(res, stocks.TickerWithData{
-	//		Ticker: d.Ticker,
-	//		DataDate: d.DataDate,
-	//		Volume: d.Volume,
-	//		High: d.High * rate.Purchase,
-	//		Low: d.Low * rate.Purchase,
-	//		Open: d.Open * rate.Purchase,
-	//		Close: d.Close * rate.Purchase,
-	//	})
-	//}
-	//
-	//return res, nil
+	res := make([]stocks.TickerWithData, 0, len(tickerDaily))
+	for _, d := range tickerDaily {
+		rates, err := i.exchangeRater.FindRates(ctx, exchanger.ConvertCurrencyParams{
+			ConvertFrom: ep.ConverFrom,
+			ConvertTo:   ep.ConvertTo,
+			Start:       d.DataDate,
+			End:         d.DataDate,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("find rate: %w", err)
+		}
+
+		// TODO: fix
+		if len(rates) != 1 {
+			continue
+		}
+
+		rate := rates[0]
+
+		res = append(res, stocks.TickerWithData{
+			Ticker:   d.Ticker,
+			DataDate: d.DataDate,
+			Volume:   d.Volume,
+			High:     d.High * rate.Purchase,
+			Low:      d.Low * rate.Purchase,
+			Open:     d.Open * rate.Purchase,
+			Close:    d.Close * rate.Purchase,
+		})
+	}
+
+	return res, nil
 }
 func (i TickerInteractor) QueryTickerDailyGraph(ctx context.Context, f stocks.QueryDailyGraphParams, ep stocks.ExchangeParams) ([]stocks.StockDailyData, error) {
 	data, err := i.repo.QueryTickerDailyGraph(ctx, i.db, f)
@@ -80,60 +81,60 @@ func (i TickerInteractor) QueryTickerDailyGraph(ctx context.Context, f stocks.Qu
 		return nil, fmt.Errorf("query daily graph: %w", err)
 	}
 
-	return data, nil
+	//return data, nil
 
-	//rates, err := i.exchangeRater.FindRates(ctx, exchanger.ConvertCurrencyParams{
-	//	ConvertFrom: ep.ConverFrom,
-	//	ConvertTo: ep.ConvertTo,
-	//	Start: data[0].Date,
-	//	End: data[len(data)-1].Date,
-	//})
-	//if err != nil {
-	//	return nil, fmt.Errorf("find rates: %w", err)
-	//}
-	//
-	//res := make([]stocks.StockDailyData, 0, len(rates))
-	//
-	//var g int
-	//var j int
-	//if len(data) == 0 {
-	//	return nil, fmt.Errorf("no graph found: %w", serrors.ErrNotFound)
-	//}
-	//if len(rates) == 0 {
-	//	return nil, fmt.Errorf("no rates found: %w", serrors.ErrNotFound)
-	//}
-	//
-	//for n := 0; n < len(data) || n < len(rates); n++ {
-	//	if data[g].Date.Equal(rates[j].Date) {
-	//		res = append(res, stocks.StockDailyData{
-	//			TickerID: data[g].TickerID,
-	//			Date: data[g].Date,
-	//			Volume: data[g].Volume,
-	//			High: data[g].High * rates[j].Purchase,
-	//			Low: data[g].Low * rates[j].Purchase,
-	//			Open: data[g].Open * rates[j].Purchase,
-	//			Close: data[g].Close * rates[j].Purchase,
-	//		})
-	//	}
-	//
-	//	if data[g].Date.Before(rates[j].Date) {
-	//		j++
-	//		continue
-	//	}
-	//
-	//	if data[g].Date.After(rates[j].Date) {
-	//		//TODO: fix
-	//		res = append(res, stocks.StockDailyData{
-	//			TickerID: data[g].TickerID,
-	//			Date: data[g].Date,
-	//			Volume: data[g].Volume,
-	//			High: data[g].High * rates[j].Purchase,
-	//			Low: data[g].Low * rates[j].Purchase,
-	//			Open: data[g].Open * rates[j].Purchase,
-	//			Close: data[g].Close * rates[j].Purchase,
-	//		})
-	//	}
-	//}
-	//
-	//return res, nil
+	rates, err := i.exchangeRater.FindRates(ctx, exchanger.ConvertCurrencyParams{
+		ConvertFrom: ep.ConverFrom,
+		ConvertTo:   ep.ConvertTo,
+		Start:       data[0].Date,
+		End:         data[len(data)-1].Date,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("find rates: %w", err)
+	}
+
+	res := make([]stocks.StockDailyData, 0, len(rates))
+
+	var g int
+	var j int
+	if len(data) == 0 {
+		return nil, fmt.Errorf("no graph found: %w", serrors.ErrNotFound)
+	}
+	if len(rates) == 0 {
+		return nil, fmt.Errorf("no rates found: %w", serrors.ErrNotFound)
+	}
+
+	for n := 0; n < len(data) || n < len(rates); n++ {
+		if data[g].Date.Equal(rates[j].Date) {
+			res = append(res, stocks.StockDailyData{
+				TickerID: data[g].TickerID,
+				Date:     data[g].Date,
+				Volume:   data[g].Volume,
+				High:     data[g].High * rates[j].Purchase,
+				Low:      data[g].Low * rates[j].Purchase,
+				Open:     data[g].Open * rates[j].Purchase,
+				Close:    data[g].Close * rates[j].Purchase,
+			})
+		}
+
+		if data[g].Date.Before(rates[j].Date) {
+			j++
+			continue
+		}
+
+		if data[g].Date.After(rates[j].Date) {
+			//TODO: fix
+			res = append(res, stocks.StockDailyData{
+				TickerID: data[g].TickerID,
+				Date:     data[g].Date,
+				Volume:   data[g].Volume,
+				High:     data[g].High * rates[j].Purchase,
+				Low:      data[g].Low * rates[j].Purchase,
+				Open:     data[g].Open * rates[j].Purchase,
+				Close:    data[g].Close * rates[j].Purchase,
+			})
+		}
+	}
+
+	return res, nil
 }
